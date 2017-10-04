8e23fccefb68f4e137ba49b1e750849a19b2f200
Angelica Zverovich <azverovich@stuy.edu>
2017-10-04 18:54:08 -0400

    WOOOOOOOOOOO

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import os
 
 my_app = Flask(__name__)
@@ -7,22 +7,29 @@ my_app.secret_key = os.urandom(32)
 
 username = "john"
 password = "doe"
+failure = ""
 
 @my_app.route('/', methods=['GET', 'POST'])
 def root():
     if (session.get(username) == password):
         return render_template('welcome.html', name = username)
+    elif (failure != ""):
+        return render_template('login.html', fail = failure)
     else:
      return render_template('login.html')
 
 @my_app.route('/submitted', methods=['GET','POST'])
 def submitted():
+    global failure
     failure = "Your login was not succesful. You entered the incorrect "
     if (request.form["name"] == username):
         if (request.form["pass"] == password):
             #success
+            failure = ""
             session["john"] = "doe"
-            return render_template('welcome.html', name = request.form["name"])
+            return redirect("/")
+
+            #return render_template('welcome.html', name = request.form["name"])
         else:
             #wrong password
             failure += "password"
@@ -32,12 +39,14 @@ def submitted():
     else:
         #wrong username AND password
         failure += "username and password"
-    return render_template("login.html", fail = failure)
+    return redirect('/')
 
 @my_app.route('/loggedout', methods=['GET','POST'])
 def logout():
-    session.pop("john")
-    return render_template("login.html")
+    logout = ""
+    if (session.get(username)==password):
+        session.pop(username)
+    return redirect("/")
 
 if __name__ == '__main__':
     my_app.debug = True
