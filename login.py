7d3e384fd0d4bdda0e29cab8881df361df3987cb
Angelica Zverovich <azverovich@stuy.edu>
2017-10-04 15:38:25 -0400

    updated to post method

--- login.py
+++ login.py
@@ -2,27 +2,27 @@ from flask import Flask, render_template, request
 
 my_app = Flask(__name__)
 
-username = "good"
-password = "bad"
+username = "john"
+password = "doe"
 failure = ""
 
-@my_app.route('/')
+@my_app.route('/', methods=['GET', 'POST'])
 def root():
     return render_template('form.html')
 
-@my_app.route('/submitted')
+@my_app.route('/submitted', methods=['GET','POST'])
 def submitted():
-    if (request.args["name"] == username):
-        if (request.args["pass"] == password):
+    if (request.form["name"] == username):
+        if (request.form["pass"] == password):
             #success
-            return render_template('success.html', name = request.args["name"])
+            return render_template('success.html', name = request.form["name"])
         else:
             failure = "password"
-    elif (request.args["pass"] == password):
+    elif (request.form["pass"] == password):
         failure = "username"
     else:
         failure = "username and password"
-    return render_template('fail.html', name = request.args["name"], fail = failure )
+    return render_template('fail.html', name = request.form["name"], fail = failure )
 
 if __name__ == '__main__':
     my_app.debug = True
